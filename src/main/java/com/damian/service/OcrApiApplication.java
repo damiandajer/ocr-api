0a1e2dade363f9cc96c930d60b3d3a55f26a05d4
damiandajer <damiandajer@gmail.com>
2021-12-29 16:02:49 +0100

    Initial commit

---
+++ src/main/java/com/damian/service/OcrApiApplication.java
@@ -0,0 +1,13 @@
+package com.damian.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrApiApplication.class, args);
+	}
+
+}
